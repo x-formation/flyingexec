2b7866d6e4f65a18e559c3246f85e302b2c53a78
Rafal Jeczalik <rjeczalik@gmail.com>
2013-12-28 14:23:01 +0100

    [wip] Forwarding client request to plugin service

--- wip/wip.go
+++ wip/wip.go
@@ -1,1 +1,125 @@
 package wip
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"net/rpc"
+	"reflect"
+)
+
+type Plugin struct{}
+
+func (p Plugin) Init(configuration []byte, res *int) error {
+	*res = 10
+	return nil
+}
+
+func StartPlugin(rcrv interface{}) (string, error) {
+	s := rpc.NewServer()
+	s.Register(rcrv)
+
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		return "", err
+	}
+	_, port, err := net.SplitHostPort(l.Addr().String())
+	if err != nil {
+		return "", err
+	}
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				continue
+			}
+			go s.ServeConn(conn)
+		}
+	}()
+	return port, nil
+}
+
+type plugind struct {
+	port    string
+	plugins map[string]string
+}
+
+func (p *plugind) ReadRequestHeader(req *rpc.Request) error {
+	return nil
+}
+
+func (p *plugind) ReadRequestBody(body interface{}) error {
+	return nil
+}
+
+func (p *plugind) WriteResponse(res *rpc.Response, body interface{}) error {
+	return nil
+}
+
+func (p *plugind) Close() error {
+	return nil
+}
+
+func NewPlugind() (*plugind, error) {
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		return nil, err
+	}
+	_, port, err := net.SplitHostPort(l.Addr().String())
+	if err != nil {
+		return nil, err
+	}
+	pd := &plugind{
+		port:    port,
+		plugins: make(map[string]string),
+	}
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				continue
+			}
+			go func() {
+				for _, port := range pd.plugins {
+					plugin, err := net.Dial("tcp", "localhost:"+port)
+					if err != nil {
+						fmt.Println("plugind error:", err)
+						continue
+					}
+					go io.Copy(conn, plugin)
+					go io.Copy(plugin, conn)
+				}
+			}()
+		}
+	}()
+	return pd, nil
+}
+
+func (pd *plugind) init(name, port string) error {
+	pd.plugins[name] = port
+	c, err := rpc.Dial("tcp", "localhost:"+port)
+	if err != nil {
+		return err
+	}
+	var cfg []byte
+	var res int
+	err = c.Call(name+".Init", cfg, &res)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (pd *plugind) Start(plugins ...interface{}) error {
+	for _, p := range plugins {
+		port, err := StartPlugin(p)
+		if err != nil {
+			return err
+		}
+		err = pd.init(reflect.TypeOf(p).Name(), port)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- wip/wip_test.go
+++ wip/wip_test.go
@@ -1,8 +1,82 @@
 package wip
 
 import (
+	"fmt"
+	"net/rpc"
+	"os"
+	"os/signal"
+	"runtime"
 	"testing"
 )
 
+func stack(full bool) string {
+	size := 1024
+	if full {
+		size = 8192
+	}
+	stack := make([]byte, size)
+	_ = runtime.Stack(stack, full)
+	return string(stack)
+}
+
+func init() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, os.Kill)
+	go func() {
+		if _, ok := <-ch; ok {
+			fmt.Printf("error: interrupted; stacktrace:\n\n%s\n", stack(true))
+			os.Exit(1)
+		}
+	}()
+}
+
+func pguard(t *testing.T) {
+	if r := recover(); r != nil {
+		t.Errorf("recovered from panic \"%v\"; stacktrace:\n\n%s", r, stack(false))
+	}
+}
+
+type MyPlugin struct {
+	Plugin
+}
+
+type MyOtherPlugin struct {
+	Plugin
+}
+
+func (p MyPlugin) Hello(req, res *string) error {
+	*res = "Hello " + *req
+	return nil
+}
+
+func (p MyOtherPlugin) Hai(req, res *string) error {
+	*res = "Hai " + *req
+	return nil
+}
+
+func (pd *plugind) call(method, req string) error {
+	c, err := rpc.Dial("tcp", "localhost:"+pd.port)
+	if err != nil {
+		return err
+	}
+	var res string
+	return c.Call(method, &req, &res)
+}
+
 func Test(t *testing.T) {
+	defer pguard(t)
+	p, err := NewPlugind()
+	if err != nil {
+		t.Fatal("newplugind:", err)
+	}
+	err = p.Start(MyPlugin{}, MyOtherPlugin{})
+	if err != nil {
+		t.Error("start:", err)
+	}
+	if err = p.call("MyPlugin.Hello", "Joe"); err != nil {
+		t.Error("call:", err)
+	}
+	if err = p.call("MyOtherPlugin.Hai", "Joe"); err != nil {
+		t.Error("call:", err)
+	}
 }
