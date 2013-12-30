3831a1655094c8157a50b7afc2d81e5874bf057b
Rafal Jeczalik <rjeczalik@gmail.com>
2013-12-30 02:57:39 +0100

    cmd/router: command line flags

--- cmd/router/main.go
+++ cmd/router/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"os"
+	"path/filepath"
 
+	"bitbucket.org/kardianos/osext"
 	"github.com/rjeczalik/gpff/router"
 )
 
-func main() {
-	rt, err := router.NewRouter()
+var addr string
+var logFile string
+var logDir string
+
+func init() {
+	dir, err := osext.ExecutableFolder()
 	if err != nil {
-		fmt.Println(err)
+		dir = "."
+	}
+	flag.StringVar(&addr, "addr", ":0", "the TCP network address for the RPC")
+	flag.StringVar(&logFile, "log", filepath.Join(dir, "router.log"), "router log file")
+	flag.StringVar(&logDir, "logdir", dir, "directory for plugin log files")
+	flag.Parse()
+}
+
+func main() {
+	rt := router.NewRouter()
+	if err := rt.ListenAndServe(addr); err != nil {
+		log.Println("router error:", err)
 		os.Exit(1)
 	}
-	_ = rt
-	select {}
 }
