16924865b942b24564d36d5527df1b3e5a9836d3
Rafal Jeczalik <rjeczalik@gmail.com>
2013-12-30 03:38:53 +0100

    cmd/router: log and logdir flags are not going to be used

--- cmd/router/main.go
+++ cmd/router/main.go
@@ -4,24 +4,14 @@ import (
 	"flag"
 	"log"
 	"os"
-	"path/filepath"
 
-	"bitbucket.org/kardianos/osext"
 	"github.com/rjeczalik/gpff/router"
 )
 
 var addr string
-var logFile string
-var logDir string
 
 func init() {
-	dir, err := osext.ExecutableFolder()
-	if err != nil {
-		dir = "."
-	}
 	flag.StringVar(&addr, "addr", ":0", "the TCP network address for the RPC")
-	flag.StringVar(&logFile, "log", filepath.Join(dir, "router.log"), "router log file")
-	flag.StringVar(&logDir, "logdir", dir, "directory for plugin log files")
 	flag.Parse()
 }
 
